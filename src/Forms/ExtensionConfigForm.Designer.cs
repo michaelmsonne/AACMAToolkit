e076d34cd8f8c037ef263bc7a0f6cd953932bb5b
Michael Morten Sonne <49366751+michaelmsonne@users.noreply.github.com>
2025-05-08 19:48:34 +0200

    Update ExtensionConfigForm.Designer.cs

--- src/Forms/ExtensionConfigForm.Designer.cs
+++ src/Forms/ExtensionConfigForm.Designer.cs
@@ -123,9 +123,9 @@
             this.Path,
             this.State});
             this.lvExtensions.HideSelection = false;
-            this.lvExtensions.Location = new System.Drawing.Point(9, 7);
+            this.lvExtensions.Location = new System.Drawing.Point(6, 7);
             this.lvExtensions.Name = "lvExtensions";
-            this.lvExtensions.Size = new System.Drawing.Size(696, 238);
+            this.lvExtensions.Size = new System.Drawing.Size(699, 238);
             this.lvExtensions.TabIndex = 7;
             this.lvExtensions.UseCompatibleStateImageBehavior = false;
             this.lvExtensions.View = System.Windows.Forms.View.Details;
@@ -252,7 +252,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(744, 334);
+            this.ClientSize = new System.Drawing.Size(747, 334);
             this.Controls.Add(this.tabControl1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
