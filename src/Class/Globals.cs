f5e2aeda8d161827bd55b9da924ca25a9fc15b5e
Michael Morten Sonne <49366751+michaelmsonne@users.noreply.github.com>
2025-05-23 19:28:00 +0200

    add dynamic code for app name in folder

--- src/Class/Globals.cs
+++ src/Class/Globals.cs
@@ -5,9 +5,10 @@ namespace AACMAToolkit.Class
     public class Globals
     {
         // Global strings used in the application
+        public static string appName = "AACMAToolkit"; // Update the application name as needed
         public static string logName = "AACMAToolkit.log";
-        public static string logPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\AACMAToolkit\\" + logName;
-        public static string logPathDynamic = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\AACMAToolkit\\" + ApplicationFunctions.GenerateDynamicLogName(logName);
+        public static string logPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\" + appName +"\\" + logName;
+        public static string logPathDynamic = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\" + appName + "\\" + ApplicationFunctions.GenerateDynamicLogName(logName);
         public static string toolLongName = "Azure Connected Machine Agent";
         public static string azcmagentPath = @"C:\Program Files\AzureConnectedMachineAgent\azcmagent.exe"; // Update the path as needed
         public static string azcmagentServiceName = "himds"; // Update the service name as needed - Azure Hybrid Instance Metadata Service
